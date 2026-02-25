162c22b644efbeeeea62a125d22d7988a7d1cdc3
JuanAlejandroRO <jar.rodriguez20@info.uas.edu.mx>
2026-02-25 15:46:58 -0700

    Actualizacion Miercoles mini proyecto

---
+++ Nomina/Controllers/PayrollController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nomina.Application;
+using Nomina.Domain;
+
+namespace Nomina.Controllers
+{
+    public class PayrollController : Controller
+    {
+        private readonly PayrollService _service = new PayrollService();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Nuevo()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Buscar(int id)
+        {
+            return View("Index");
+        }
+
+        public IActionResult Modificar(Employee employee)
+        {
+            return View("Index");
+        }
+
+        public IActionResult Eliminar(int id)
+        {
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(Employee employee)
+        {
+            var result = _service.Calculate(employee);
+            return View("Result", result);
+        }
+    }
+}
---
+++ Nomina/Domain/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace Nomina.Domain
+{
+    public enum Role
+    {
+        Chofer,
+        Cargador,
+        Auxiliar
+    }
+
+    public enum EmployeeType
+    {
+        Interno,
+        Subcontratado
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public Role Role { get; set; }
+        public EmployeeType Type { get; set; }
+        public int HoursWorked { get; set; }
+        public int Deliveries { get; set; }
+        public Role? CoveredRole { get; set; }
+    }
+}
